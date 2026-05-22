8bf980648fce909dbfe5ed2712079690e0ec347a
root <root@srv1489383.hstgr.cloud>
2026-05-22 09:38:33 +0000

    Fix route aliases and remove env backups

--- src/app/article/[slug]/page.tsx
+++ src/app/article/[slug]/page.tsx
@@ -1,12 +1,6 @@
-import Page, {
+import Page from "../../articles/[slug]/page";
 
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
-  generateMetadata as generateAliasMetadata,
-  generateStaticParams as generateAliasStaticParams,
-} from "../../articles/[slug]/page";
-
 export const revalidate = 3;
-export const generateMetadata = generateAliasMetadata;
-export const generateStaticParams = generateAliasStaticParams;
 export default Page;
--- src/app/listing/[slug]/page.tsx
+++ src/app/listing/[slug]/page.tsx
@@ -1,12 +1,6 @@
-import Page, {
+import Page from "../../listings/[slug]/page";
 
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
-  generateMetadata as generateAliasMetadata,
-  generateStaticParams as generateAliasStaticParams,
-} from "../../listings/[slug]/page";
-
 export const revalidate = 3;
-export const generateMetadata = generateAliasMetadata;
-export const generateStaticParams = generateAliasStaticParams;
 export default Page;
